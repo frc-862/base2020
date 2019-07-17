e521c9085a53f721e7e1ad7cd2b024de89bd8a72
Shane Hurley <shane@hurleyhome.com>
2019-07-17 17:47:19 -0400

    Format code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,108 +24,108 @@ import edu.wpi.first.wpilibj.experimental.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static Drivetrain drivetrain;
-  public static Core core;
-  public static OI oi;
+    public static Drivetrain drivetrain;
+    public static Core core;
+    public static OI oi;
 
-  Command autonomousCommand;
-  SendableChooser<Command> chooser = new SendableChooser<>();
+    Command autonomousCommand;
+    SendableChooser<Command> chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    drivetrain = new Drivetrain();
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    @Override
+    public void robotInit() {
+        drivetrain = new Drivetrain();
 //    core = new Core();
 
-    oi = new OI();
-    //m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    SmartDashboard.putData("Auto mode", chooser);
-  }
+        oi = new OI();
+        //m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+        // chooser.addOption("My Auto", new MyAutoCommand());
+        SmartDashboard.putData("Auto mode", chooser);
+    }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
-
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
-  @Override
-  public void disabledInit() { }
-
-  @Override
-  public void disabledPeriodic() { }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
-   */
-  @Override
-  public void autonomousInit() {
-    autonomousCommand = chooser.getSelected();
-
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
+    /**
+     * This function is called every robot packet, no matter the mode. Use
+     * this for items like diagnostics that you want ran during disabled,
+     * autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before
+     * LiveWindow and SmartDashboard integrated updating.
      */
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
 
-    // schedule the autonomous command (example)
-    if (autonomousCommand != null) {
+    /**
+     * This function is called once each time the robot enters Disabled mode.
+     * You can use it to reset any subsystem information you want to clear when
+     * the robot is disabled.
+     */
+    @Override
+    public void disabledInit() { }
+
+    @Override
+    public void disabledPeriodic() { }
+
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString code to get the auto name from the text box below the Gyro
+     *
+     * <p>You can add additional auto modes by adding additional commands to the
+     * chooser code above (like the commented example) or additional comparisons
+     * to the switch structure below with additional strings & commands.
+     */
+    @Override
+    public void autonomousInit() {
+        autonomousCommand = chooser.getSelected();
+
+        /*
+         * String autoSelected = SmartDashboard.getString("Auto Selector",
+         * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+         * = new MyAutoCommand(); break; case "Default Auto": default:
+         * autonomousCommand = new ExampleCommand(); break; }
+         */
+
+        // schedule the autonomous command (example)
+        if (autonomousCommand != null) {
 //      autonomousCommand.
+        }
     }
-  }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() { }
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() { }
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
+    @Override
+    public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) {
+            autonomousCommand.cancel();
+        }
     }
-  }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() { }
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() { }
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
 
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -17,74 +17,74 @@ import java.util.function.Consumer;
 
 public class Drivetrain extends SendableSubsystemBase
 {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-  CANSparkMax left1 = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
-  CANSparkMax left2 = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
-  CANSparkMax left3 = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    CANSparkMax left1 = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax left2 = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax left3 = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-  CANSparkMax right1 = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
-  CANSparkMax right2 = new CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
-  CANSparkMax right3 = new CANSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax right1 = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax right2 = new CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax right3 = new CANSparkMax(6, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-  public Drivetrain() {
-    left2.setInverted(true);
-    right1.setInverted(true);
-    right3.setInverted(true);
+    public Drivetrain() {
+        left2.setInverted(true);
+        right1.setInverted(true);
+        right3.setInverted(true);
 
-    withEachMotor((m) -> m.setOpenLoopRampRate(0.5));
+        withEachMotor((m) -> m.setOpenLoopRampRate(0.5));
 
-    setDefaultCommand(new ArcadeDrive(this));
-  }
+        setDefaultCommand(new ArcadeDrive(this));
+    }
 
-  private void withEachMotor(Consumer<CANSparkMax> op) {
-    op.accept(left1);
-    op.accept(left2);
-    op.accept(left3);
-    op.accept(right1);
-    op.accept(right2);
-    op.accept(right3);
-  }
+    private void withEachMotor(Consumer<CANSparkMax> op) {
+        op.accept(left1);
+        op.accept(left2);
+        op.accept(left3);
+        op.accept(right1);
+        op.accept(right2);
+        op.accept(right3);
+    }
 
-  public void setPower(double left, double right) {
-    left *= 0.5;
-    right *= 0.5;
+    public void setPower(double left, double right) {
+        left *= 0.5;
+        right *= 0.5;
 
-    left1.set(left);
-    left2.set(left);
+        left1.set(left);
+        left2.set(left);
 //    left3.set(left);
 
-    right1.set(right);
-    right2.set(right);
+        right1.set(right);
+        right2.set(right);
 //    right3.set(right);
-  }
+    }
 
-  public void setVelocity(double left, double right) {
-    // TODO implement
-  }
+    public void setVelocity(double left, double right) {
+        // TODO implement
+    }
 
-  public void resetDistance() {
-      // TODO implement
-  }
+    public void resetDistance() {
+        // TODO implement
+    }
 
-  public double getLeftDistance() {
-    // TODO implement
-    return 0;
-  }
+    public double getLeftDistance() {
+        // TODO implement
+        return 0;
+    }
 
-  public double getRightDistance() {
-    // TODO implement
-    return 0;
-  }
+    public double getRightDistance() {
+        // TODO implement
+        return 0;
+    }
 
-  public double getLeftVelocity() {
-    // TODO implement
-    return 0;
-  }
+    public double getLeftVelocity() {
+        // TODO implement
+        return 0;
+    }
 
-  public double getRightVelocity() {
-      // TODO implement
-      return 0;
-  }
+    public double getRightVelocity() {
+        // TODO implement
+        return 0;
+    }
 }
 
